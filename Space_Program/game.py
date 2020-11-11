52f88762102d720bf83df7cf394c4e3a06206f59
Gubarev Egor <gubarev.ed@phystech.edu>
2020-11-11 15:09:33 +0300

    Button Tutorials in menu is working

--- Space_Program/game.py
+++ Space_Program/game.py
@@ -13,30 +13,34 @@ class Game:
         self.clock = pygame.time.Clock()
         self.color_dict: dict = {'black': (0, 0, 0)}
         self.buttons_dict: dict = {'create': [None],
-                                   'close': [[button_cancel, game_close_cancel],
-                                             [button_close, game_exit]],
-                                   'credits': [[button_credits_close, game_close_cancel]],
-                                   'menu': [[button_credits, game_credits],
-                                            [button_exit, game_close],
-                                            [button_play, None],
-                                            [button_setting, None],
-                                            [button_tutorials, None]]}
+                                   'close': [[button_close_cancel, game_menu],
+                                             [button_close_close, game_exit]],
+                                   'credits': [[button_credits_close, game_menu]],
+                                   'menu': [[button_menu_credits, game_credits],
+                                            [button_menu_close, game_close],
+                                            [button_menu_play, None],
+                                            [button_menu_setting, None],
+                                            [button_menu_tutorials, game_tutorials]],
+                                   'tutorials': [[button_tutorials_close, game_menu]]}
         self.graphic_objects_dict: dict = {'create': [None],
                                            'close': [background,
-                                                     button_cancel,
-                                                     button_close,
+                                                     button_close_cancel,
+                                                     button_close_close,
                                                      text_close],
                                            'credits': [background,
                                                        button_credits_close,
                                                        text_credits_egorka,
                                                        text_credits_polina],
                                            'menu': [background,
-                                                    button_credits,
-                                                    button_exit,
-                                                    button_play,
-                                                    button_setting,
-                                                    button_tutorials,
-                                                    text_menu]}
+                                                    button_menu_credits,
+                                                    button_menu_close,
+                                                    button_menu_play,
+                                                    button_menu_setting,
+                                                    button_menu_tutorials,
+                                                    text_menu],
+                                           'tutorials': [background,
+                                                         text_tutorials,
+                                                         button_tutorials_close]}
         self.fps: int = 24  # in [Hz]
         self.mode: str = 'create'
         self.screen_height: int = 700  # in [px]
