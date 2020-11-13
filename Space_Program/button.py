b9373a254b1352097683c082b638e2166bb8edd4
Gubarev Egor <gubarev.ed@phystech.edu>
2020-11-14 00:07:16 +0300

    Fps button in settings is working

--- Space_Program/button.py
+++ Space_Program/button.py
@@ -6,7 +6,7 @@ from pygame.font import *
 
 class Button:
 
-    def __init__(self, parameter: any, values_list: list, texts_list: list, x: int, y: int):
+    def __init__(self, parameter: any, texts_list: list, values_list: list, x: int, y: int):
         """
         Zero point is top left
         x in [px]
@@ -16,9 +16,6 @@ class Button:
         # Button
         self.color: tuple = (0, 0, 128)
         self.parameter: any = parameter
-        self.values: list = values_list
-        self.value_index: int = 0
-        self.value: any = values_list[self.value_index]
         self.x: int = x
         self.y: int = y
 
@@ -30,6 +27,11 @@ class Button:
         self.texts: list = texts_list
         self.text_str_index: int = 0
         self.text_str: str = texts_list[self.text_str_index]
+
+        # Value
+        self.values_list: list = values_list
+        self.value_index: int = 0
+        self.value: any = values_list[self.value_index]
 
     def draw(self, screen):
 
@@ -59,7 +61,8 @@ class Button:
             # Button
             height: int = self.font_size  # in [px]
             value_index: int = self.value_index
-            values_amount: int = len(self.values)
+            values_list: list = self.values_list
+            values_amount: int = len(values_list)
             width: int = len(self.text_str) * self.font_size  # in [px]
             x: int = self.x  # in [px]
             y: int = self.y  # in [px]
@@ -72,11 +75,15 @@ class Button:
             texts_str_amount: int = len(self.texts)
             text_str_index: int = self.text_str_index
 
+            # Value
+            self.values_list: list = values_list
+
             if x <= mouse_x <= x + width and y <= mouse_y <= y + height:
                 raw_text_str_index: int = text_str_index + 1
                 raw_value_index: int = value_index + 1
                 self.text_str_index: int = raw_text_str_index % texts_str_amount
                 self.value_index: int = raw_value_index % values_amount
+                self.value: any = values_list[value_index]
                 return True
             else:
                 return False
