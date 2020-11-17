d17bb128ca06c0af308d6135e7061137569192f2
Gubarev Egor <gubarev.ed@phystech.edu>
2020-11-17 09:40:03 +0300

    Refactoring is done

--- Forest_Survival/apple.py
+++ Forest_Survival/apple.py
@@ -2,26 +2,38 @@
 Модуль яблока
 """
 
+from pygame.draw import *
+
 
 class Apple(object):
     """
     Описывает яблоко
     """
 
-    def __init__(self, x: float, y: float):
+    def __init__(self, screen, x: float, y: float):
         """
         Параметры
 
-        Физика
         Координаты центра яблока
+
         x - координата x яблока в [м]
         y - координата y яблока в [м]
-
-        Графика
         screen - экран pygame
         """
 
         self.color: tuple = (5, 95, 23)  # Цвет яблока
         self.radius: int = 5  # Радиус яблока в [px]
+        self.screen = screen
         self.x: float = x
         self.y: float = y
+
+    # --- Графика ---
+    def draw(self, x: int, y: int):
+        """
+        Рисует яблоко
+
+        x - координата яблока в [px]
+        y - координата яблока в [px]
+        """
+
+        circle(self.screen, self.color, (x, y), self.radius)
--- Forest_Survival/forest.py
+++ Forest_Survival/forest.py
@@ -15,37 +15,16 @@ class Forest(object):
     Описывает лес
     """
 
-    def __init__(self, hero_x: float, hero_y: float, screen):
+    def __init__(self, hero, screen):
         """
         Параметры
 
-        Физика
-        Координаты героя отсчитываются от его центра
+        Координаты центров объектов
         Словарь прямой состоит из координаты, которую имеет прямая, и её значения
-        hero_x - Координата x героя в [м]
-        hero_y - Координата y героя в [м]
 
-        Графика
+        hero - объект героя
         screen - экран pygame
         """
-
-        # Логика
-        self.apples_list: list = []  # Список яблок
-        self.scale: int = 35  # Масштаб в [px/м]
-
-        # Физика
-        self.down_border_dict: dict = {'coordinate': 'y',
-                                       'value': 10}  # Словарь нижней границы леса
-        self.hero_x: float = hero_x
-        self.hero_y: float = hero_y
-        self.left_border_dict: dict = {'coordinate': 'x',
-                                       'value': -10}  # Словаь левой границы леса
-        self.right_border_dict: dict = {'coordinate': 'x',
-                                        'value': 10}  # Словарь правой границы леса
-        self.up_border_dict: dict = {'coordinate': 'y',
-                                     'value': -10}  # Словарь верхней границы леса
-        self.borders_list: list = [self.down_border_dict, self.left_border_dict, self.right_border_dict,
-                                   self.up_border_dict]
 
         # Графика
         self.border_color: tuple = (185, 250, 250)  # Цвет границ
@@ -53,83 +32,161 @@ class Forest(object):
         self.color: tuple = (193, 86, 217)  # Цвет леса
         self.screen = screen
 
+        # Логика
+        self.scale: int = 35  # Масштаб в [px/м]
+        self.max_draw_distance = None  # Максимальное расстояние прорисовки в [м] определяется в forest.setup()
+        self.items_list: list = []  # Список объектов
+
+        # Физика
+        self.borders_dict = None  # Словари границ определяется в forest.setup()
+        self.max_x_distance = None  # Горизонтальный размер экрана в [м] определяется в forest.setup()
+        self.max_y_distance = None  # Вертикальный размер экрана в [м] определяется в forest.setup()
+
+        # Объекты
+        self.hero = hero
+
     # --- Логика ---
+    def convert_horizontal_m_to_px(self, coordinate_m: float):
+        """
+        Преобразует [м] в [px] по горизонтали
+
+        coordinate_m - координата объекта в [м]
+        """
+
+        distance_m: float = coordinate_m - self.hero.x  # Расстояние от героя до объекта в [м]
+        distance_raw: float = distance_m * self.scale  # Расстояние в [px]
+        distance_px: int = round(distance_raw)  # Округлённое расстояние в [px]
+        distance_px_cooked: int = distance_px + self.screen.get_width() // 2  # Координата  объекта в [px]
+        return distance_px_cooked
+
+    def convert_vertical_m_to_px(self, coordinate_m: float):
+        """
+        Преобразует [м] в [px] по вертикали
+
+        coordinate_m - координата объекта в [м]
+        """
+
+        distance_m: float = coordinate_m - self.hero.y  # Расстояние от героя до объекта в [м]
+        distance_raw: float = distance_m * self.scale  # Расстояние в [px]
+        distance_px: int = round(distance_raw)  # Округлённое расстояние в [px]
+        distance_px_cooked: int = distance_px + self.screen.get_height() // 2  # Координата  объекта в [px]
+        return distance_px_cooked
+
+    def count_draw_distance(self):
+        """
+        Вычисляет расстояние прорисовки в [м]
+        """
+
+        screen_height: int = self.screen.get_height()  # Высота экрана в [px]
+        screen_width: int = self.screen.get_width()  # Ширина экрана в [px]
+
+        # Максимальное расстояние на экране в [px]
+        max_screen_distance: float = math.sqrt(screen_height ** 2 + screen_width ** 2)
+
+        max_distance: float = max_screen_distance / self.scale  # Максимальное расстояние до видимого объекта в [м]
+        self.max_draw_distance: int = math.ceil(max_distance)  # Максимальное расстояние прорисовки в []
+
     def setup(self):
         """
         Действия при создании леса
         """
 
-        self.generate_apples()
+        self.create_borders()
+        self.count_max_distance()
+        self.count_draw_distance()
+        self.generate_items()
 
     # --- Физика ---
     def calculate_distance_to_line(self, line_dict: dict):
         """
         Вычисляет расстояние от героя до прямой
 
-        Физика
         line_dict - словарь прямой
         """
 
-        if line_dict['coordinate'] == 'x':  # Если необходимо посчитать расстояние вдоль x
-            hero_coordinate: float = self.hero_x  # Координата x героя в [м]
-        elif line_dict['coordinate'] == 'y':  # Если необходимо посчитать расстояние вдоль y
-            hero_coordinate: float = self.hero_y  # Координата y героя в [м]
-        else:
-            return None
+        coordinate_dict: dict = {'x': self.hero.x,  # Словарь координат
+                                 'y': self.hero.y}
+        coordinate: str = line_dict['coordinate']  # Координата, вдоль которой надо считать расстяние
+        hero_coordinate: float = coordinate_dict[coordinate]  # Координата x героя в [м]
         distance: float = line_dict['value'] - hero_coordinate  # Расстояние от героя до прямой в [м]
         return distance
 
     def calculate_distance_to_point(self, x: float, y: float):
         """
-        Вычисляет расстояние от героя до точки
+        Вычисляет компоненты расстояния от героя до точки
 
-        Физика
         x - Координата x точки в [м]
         y - Координата y точки в [м]
         """
 
-        hero_x: float = self.hero_x  # Координата x героя в [м]
-        hero_y: float = self.hero_y  # Координата y героя в [м]
-        x_distance: float = x - hero_x  # Расстояние от героя до точки вдоль оси x в [м]
-        y_distance: float = y - hero_y  # Расстояние от героя до точки вдоль оси y в [м]
+        x_distance: float = x - self.hero.x  # Расстояние от героя до точки вдоль оси x в [м]
+        y_distance: float = y - self.hero.y  # Расстояние от героя до точки вдоль оси y в [м]
         distance_list: list = [x_distance, y_distance]  # Компоненты расстояния от героя до точки в [м]
         return distance_list
 
-    def check_border_distance(self, border_dict: dict):
+    def count_max_distance(self):
         """
-        Проверяет расстояни от игрока до границы
-
-        Физика
-        border_dict - словарь границы
+        Вычисляет размеры экрана в [м]
         """
 
-        border_distance: float = self.calculate_distance_to_line(border_dict)
-        return border_distance
-
-    # --- Объекты ---
-    def generate_apples(self):
-        """
-        Создаёт яблоки
-        """
-
-        # Физика
-        down_border_dict: dict = self.down_border_dict  # Словарь нижней границы
-        left_border_dict: dict = self.left_border_dict  # Словарь левой границы
-        right_border_dict: dict = self.right_border_dict  # Словарь правой границы
-        up_border_dict: dict = self.up_border_dict  # Словарь верхней границы
+        down_border_dict: dict = self.borders_dict['down']  # Словарь нижней границы
+        left_border_dict: dict = self.borders_dict['left']  # Словарь левой границы
+        right_border_dict: dict = self.borders_dict['right']  # Словарь правой границы
+        up_border_dict: dict = self.borders_dict['up']  # Словарь верхней границы
         x_left: float = left_border_dict['value']  # Координата x левой границы в [м]
         x_right: float = right_border_dict['value']  # Координата x правой границы в [м]
-        x_distance: float = x_right - x_left  # Расстояние между границами вдоль оси x в [м]
         y_down: float = down_border_dict['value']  # Координата y нижней границы в [м]
         y_up: float = up_border_dict['value']  # Координата y верхней границы в [м]
-        y_distance: float = y_down - y_up  # Расстояние между границами по y в [м]
+        self.max_x_distance: float = x_right - x_left  # Расстояние между границами вдоль оси x в [м]
+        self.max_y_distance: float = y_down - y_up  # Расстояние между границами по y в [м]
+
+    def create_borders(self):
+        """
+        Создаёт словарь границ
+        """
+
+        down_border_dict: dict = {'coordinate': 'y',
+                                  'value': 10}  # Словарь нижней границы леса
+        left_border_dict: dict = {'coordinate': 'x',
+                                  'value': -10}  # Словаь левой границы леса
+        right_border_dict: dict = {'coordinate': 'x',
+                                   'value': 10}  # Словарь правой границы леса
+        up_border_dict: dict = {'coordinate': 'y',
+                                'value': -10}  # Словарь верхней границы леса
+        self.borders_dict: dict = {'down': down_border_dict,  # Словарь границ
+                                   'left': left_border_dict,
+                                   'right': right_border_dict,
+                                   'up': up_border_dict}
+
+    def draw_needed(self, distance: float):
+        """
+        Проверяет, нужно ли рисовать объект
+
+        line_dict - словарь прямой
+        """
+
+        if distance <= self.max_draw_distance:  # Если прямую надо прорисовывать
+            return True
+        else:
+            return False
+
+    # --- Объекты ---
+    def generate_items(self):
+        """
+        Создаёт объекты
+        """
 
         apples_amount: int = 5  # Количество яблок
         for apple_number in range(apples_amount):
-            x_apple: float = random() * x_distance + x_left  # Координата x яблока в [м]
-            y_apple: float = random() * y_distance + y_up  # Координата y яблока в [м]
-            apple = Apple(x_apple, y_apple)  # Объект яблока
-            self.apples_list.append(apple)
+
+            # Координата x яблока в [м]
+            x_apple: float = random() * self.max_x_distance + self.borders_dict['left']['value']
+
+            # Координата y яблока в [м]
+            y_apple: float = random() * self.max_y_distance + self.borders_dict['up']['value']
+
+            apple = Apple(self.screen, x_apple, y_apple)  # Объект яблока
+            self.items_list.append(apple)
 
     # --- Графика ---
     def draw_background(self):
@@ -137,76 +194,31 @@ class Forest(object):
         Рисует фон леса
         """
 
-        # Графика
-        color: tuple = self.color  # Цвет леса
-        screen = self.screen  # Экран pygame
+        self.screen.fill(self.color)
 
-        screen.fill(color)
-
-    def draw_line(self, coordinate: str, line_distance: float):
+    def draw_horizontal_line(self, line_coordinate: float):
         """
-        Рисует прямую
+        Рисует горизонтальную прямую
 
-        Физика
-        coordinate - Координата, вдоль которой рисовать прямую
-        line_distance - Расстояние от героя до прямой в [м]
+        line_coordinate - Координата прямой в [м]
         """
 
-        # Логика
-        scale: int = self.scale  # Масшаб в [px/м]
+        y_1: int = -1  # Верхняя точка выше экрана
+        y_2: int = self.screen.get_height() + 1  # Нижняя точка ниже экрана
 
-        # Графика
-        line_color: tuple = self.border_color  # Цвет линии
-        line_width: int = self.border_width  # Толщина линии в [px]
-        screen = self.screen  # Экран pygame
-        screen_height: int = screen.get_height()  # Высота экрана в [px]
-        screen_width: int = screen.get_width()  # Ширина экрана в [px]
+        line(self.screen, self.border_color, [line_coordinate, y_1], [line_coordinate, y_2], self.border_width)
 
-        if coordinate == 'x':  # Если нужно отрисовать вертикальную границу
-            line_x_raw: float = line_distance * scale  # Расстояние от героя до прямой в [px]
-            line_x: int = round(line_x_raw)  # Округлённое расстояние от героя до прямой в [px]
-            line_x_cooked: int = line_x + screen_width // 2  # Координата x прямой в [px]
-            line_y_1: int = -1  # Верхняя точка выше экрана
-            line_y_2: int = screen_height + 1  # Нижняя точка ниже экрана
-
-            line(screen, line_color, [line_x_cooked, line_y_1], [line_x_cooked, line_y_2], line_width)
-
-        elif coordinate == 'y':  # Если нужно отрисовать горизонтальную границу
-            line_x_1: int = -1  # Левая точка левее экрана
-            line_x_2: int = screen_width + 1  # Правая точка правее экрана
-            line_y_raw: float = line_distance * scale  # Расстояние от героя до прямой в [px]
-            line_y: int = round(line_y_raw)  # Округлённое расстояние от героя до прямой в [px]
-            line_y_cooked: int = line_y + screen_height // 2  # Координата y прямой в [px]
-
-            line(screen, line_color, [line_x_1, line_y_cooked], [line_x_2, line_y_cooked], line_width)
-
-    def draw_object(self, item, item_distance_x: float, item_distance_y: float):
+    def draw_vertical_line(self, line_coordinate: float):
         """
-        Рисует объект
+        Рисует вертикальную прямую
 
-        item - объект, который нужно нарисовать
-        item_distance_x - Расстояние от героя до точки вдоль оси x в [м]
-        item_distance_y - Расстояние от героя до точки вдоль оси y в [м]
+        line_coordinate - Координата прямой в [м]
         """
 
-        # Логика
-        scale: int = self.scale  # Масшаб в [px/м]
+        x_1: int = -1  # Левая точка левее экрана
+        x_2: int = self.screen.get_width() + 1  # Правая точка правее экрана
 
-        # Графика
-        color: tuple = item.color  # Цвет объекта
-        radius: int = item.radius  # Радиус объекта в [px]
-        screen = self.screen  # Экран pygame
-        screen_height: int = screen.get_height()  # Высота экрана в [px]
-        screen_width: int = screen.get_width()  # Ширина экрана в [px]
-
-        x_raw: float = item_distance_x * scale  # Расстояние от героя до точки вдоль оси x в [px]
-        item_x: int = round(x_raw)  # Округлённое расстояние от героя до точки вдоль оси x в [px]
-        item_x_cooked: int = item_x + screen_width // 2  # Координата x точки в [px]
-        y_raw: float = item_distance_y * scale  # Расстояние от героя до точки вдоль оси y в [px]
-        item_y: int = round(y_raw)  # Округлённое расстояние от героя до точки вдоль оси y в [px]
-        item_y_cooked: int = item_y + screen_height // 2  # Координата y точки в [px]
-
-        circle(screen, color, (item_x_cooked, item_y_cooked), radius)
+        line(self.screen, self.border_color, [x_1, line_coordinate], [x_2, line_coordinate], self.border_width)
 
     # --- Обработка ---
     def process(self):
@@ -214,43 +226,20 @@ class Forest(object):
         Обрабатывает события леса
         """
 
-        # Графика
-        screen = self.screen  # Экран pygame
-        screen_height: int = screen.get_height()  # Высота экрана в [px]
-        screen_width: int = screen.get_width()  # Ширина экрана в [px]
-
-        # Максимальное расстояние от героя то точки экрана
-        max_distance: float = math.sqrt(screen_height ** 2 + screen_width ** 2)
-
-        # Логика
-        apples_list: list = self.apples_list  # Список яблок
-        scale: int = self.scale  # Масштаб в [px/м]
-        max_draw_distance_raw: float = max_distance // scale  # Максимальное расстояние прорисовки объекта в [м]
-
-        # Дистанция, в пределах которой объекты отрисовываются в [м]
-        max_draw_distance: int = math.ceil(max_draw_distance_raw)
-
-        # Физика
-        borders_list: list = self.borders_list  # Список границ
-
         self.draw_background()
-        for border_dict in borders_list:
+        for border_direction in self.borders_dict:
+            border_dict: dict = self.borders_dict[border_direction]  # Словарь границы
             border_distance: float = self.calculate_distance_to_line(border_dict)  # Расстояние до границы в [м]
-
-            # Если расстяние до границы не превышает расстояния прорисовки
-            if abs(border_distance) <= max_draw_distance:
-                coordinate: str = border_dict['coordinate']  # Координата, вдоль которой рисовать прямую
-                self.draw_line(coordinate, border_distance)
-        for apple in apples_list:
-            x_apple: float = apple.x  # Координата x яблока в [м]
-            y_apple: float = apple.y  # Координата y яблока в [м]
-            apple_distance_list: list = self.calculate_distance_to_point(x_apple, y_apple)
-            apple_distance_x: float = apple_distance_list[0]  # Расстояние от героя до точки вдоль оси x в [м]
-            apple_distance_y: float = apple_distance_list[1]  # Расстояние от героя до точки вдоль оси y в [м]
-
-            # Расстояние от героя до яблока в [м]
-            apple_distance: float = math.sqrt(apple_distance_x ** 2 + apple_distance_y ** 2)
-
-            # Если расстяние до яблока не превышает расстояния прорисовки
-            if abs(apple_distance) <= max_draw_distance:
-                self.draw_object(apple, apple_distance_x, apple_distance_y)
+            if self.draw_needed(border_distance):
+                coordinate_m = border_dict['value']  # Координата грпницы в [м]
+                axis: str = border_dict['coordinate']  # Ось, вдоль которой рисовать границу
+                if axis == 'x':  # Если прямая горизонтальна
+                    coordinate_px: int = self.convert_horizontal_m_to_px(coordinate_m)  # Координата границы в [px]
+                    self.draw_horizontal_line(coordinate_px)
+                elif axis == 'y':  # Если прямая вертикальна
+                    coordinate_px: int = self.convert_vertical_m_to_px(coordinate_m)  # Координата границы в [px]
+                    self.draw_vertical_line(coordinate_px)
+        for item in self.items_list:
+            item_x: int = self.convert_horizontal_m_to_px(item.x)  # Координата объекта по оси x в [px]
+            item_y: int = self.convert_vertical_m_to_px(item.y)  # Координата объекта по оси y в [px]
+            item.draw(item_x, item_y)
